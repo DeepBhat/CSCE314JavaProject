ca50915534700d8b3af00c4c9b2dad63337a4caa
Deepansh Bhatia <deepanshbhatia@tamu.edu>
2019-11-26 12:07:52 -0600

    FileAccess part done

--- FileAccess.java
+++ FileAccess.java
@@ -9,21 +9,52 @@ import java.util.Scanner;
 
 public class FileAccess {
   
-  public static boolean loadPrimes(Primes primes, String filename) {
+  public static boolean loadPrimes(Primes primes, String filename) throws FileNotFoundException {
+    File file = new File(Config.DATAPATH + filename); 
+    Scanner scanner = new Scanner(file); 
+    while (scanner.hasNextLine()) {
+      primes.addPrime(scanner.nextBigInteger());
+    }
+    scanner.close();
 		return true;
   }
   
-  public static boolean loadCrosses(Primes primes, String filename) {
+  public static boolean loadCrosses(Primes primes, String filename) throws FileNotFoundException {
+    File file = new File(Config.DATAPATH + filename);
+    Scanner scanner = new Scanner(file);
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      BigInteger crossOne = new BigInteger(line.substring(0,line.indexOf(",")));
+      BigInteger crossTwo = new BigInteger(line.substring(line.indexOf(", ") + 1));
+      Pair<BigInteger> firstPair = new Pair<BigInteger>(crossOne.subtract(BigInteger.ONE), crossOne.add(BigInteger.ONE));
+      Pair<BigInteger> secondPair = new Pair<BigInteger>(crossTwo.subtract(BigInteger.ONE), crossTwo.add(BigInteger.ONE));
+      primes.addCross(new Pair<Pair<BigInteger>>(firstPair, secondPair));
+    }
+    scanner.close();
     return true;
 	}
   
-  public static boolean savePrimes(Primes primes, String filename)
-  {
+  public static boolean savePrimes(Primes primes, String filename) throws IOException {
+    File file = new File(Config.DATAPATH + filename);
+    FileWriter writer = new FileWriter(file);
+
+    for (BigInteger prime : primes.iteratePrimes()) {
+      String writeLine = prime.toString() + "\n";
+      writer.write(writeLine);
+    }
+    writer.close();
   	return true;
   }
   
-  public static boolean saveCrosses(Primes primes, String filename)
-  {
+  public static boolean saveCrosses(Primes primes, String filename) throws IOException {
+    File file = new File(Config.DATAPATH + filename);
+    FileWriter writer = new FileWriter(file);
+    
+    for (Pair<BigInteger> middlePair : primes.iterateCrosses()) {
+      String writeLine = middlePair.toString() + "/n";
+      writer.write(writeLine);
+    }
+    writer.close();
   	return true;
   }
 }
